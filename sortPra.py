0f71ff9cfa6dce99e6741c9dadd32e8943c3b5fb
hustegg <hustegg@gmail.com>
2014-10-21 12:07:37 +0800

    Update sortPra.py

--- sortPra.py
+++ sortPra.py
@@ -64,8 +64,33 @@ def insertSort(*inList):
         i += 1
     return newList
 
+def shellSort(*inList):
+    newList = list(inList)
+    l = len(newList)
+    gap = l / 2
+    while gap > 0:
+        i = gap
+        while i < l:
+            temp = newList[i]
+            j = i
+            while j >= gap:
+                if temp > newList[j - gap]:
+                    break
+                newList[j] = newList[j - gap]
+                j -= gap
+            newList[j] = temp
+            i += 1
+        gap /= 2
+    
+    return newList     
+    
+
+
+
+
 if __name__ == '__main__':
     print insertSort(314,15,926,53,5897,9323,84626,43383,279,502,88,41,97,16939,93,75,1058,209,74,9,445,9,23,07,8,16,40,62,862,0,8,99,8628,0,34,8,2,53)
+    print shellSort(314,15,926,53,5897,9323,84626,43383,279,502,88,41,97,16939,93,75,1058,209,74,9,445,9,23,07,8,16,40,62,862,0,8,99,8628,0,34,8,2,53)
 
 
 
