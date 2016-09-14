fc07db38179c5616713bdf97da73a8c3d65bb625
hustegg <hustegg@gmail.com>
2016-09-14 13:01:03 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -2,6 +2,7 @@
 
 from MySQLdb import connect
 from optparse import OptionParser
+from datetime import datetime
 import traceback
 import MySQLdb.cursors
 import xlwt
@@ -22,6 +23,8 @@ def parseOptions():
     parser.add_option('-u', '--user', dest='USER', help='username default query user')
     parser.add_option('-p', '--password', dest='PSWD', help='password default query password')
     parser.add_option('-d', '--database', dest='DATABASE', help='table name database')
+    parser.add_option('-c', '--charset', dest='CHARSET', default='gb2312', help='charset of text')
+    parser.add_option('-s', '--db-charset', dest='DBCHARSET', default='latin1', help='charset of connection')
     parser.add_option('-t', '--table', dest='TABLE', help='table name')
     parser.add_option('-w', '--where', dest='WHERE', default=None, help='condition in where')
     parser.add_option('-i', '--infile', dest='INFILE', default='table.xls', help='input file name')
@@ -37,17 +40,26 @@ def download(inCur, inSQL):
     vHeader = tuple([ i[0] for i in vCur.description ])
     vHType = tuple([ i[1] for i in vCur.description ])
     vRecords = vCur.fetchall()
-    
+    vColWidMap = gConf['COLWIDTH']
+
     vBook = xlwt.Workbook()
     vSheet = vBook.add_sheet('sheet1')
-    
+
+    for i, t in enumerate(vHType):
+        vColSetWidth = vSheet.col(i)
+        vColSetWidth.width = max(vColWidMap[vHType[i]], len(vHeader[i]) + 1) * 256
+
     for ic, cell in enumerate(vHeader):
         vSheet.write(0, ic, cell)
     
     for ir, row in enumerate(vRecords, 1):
         for ic, cell in enumerate(row):
-            if not isinstance(cell, unicode):
+            if isinstance(cell, datetime):
                 cellStr = str(cell)
+            elif isinstance(cell, unicode):
+                cellStr = cell.encode('raw_unicode_escape').decode(gConf['CHARSET'])
+            elif isinstance(cell, str):
+                cellStr = cell.strip()
             else:
                 cellStr = cell
             vSheet.write(ir, ic, cellStr, gConf['XF'].get(vHType[ic], gConf['XF'][-1]))
@@ -65,12 +77,13 @@ def upload(inCur):
     vColName = vSheet.row_values(0)
     vSQLFmt = "INSERT INTO {0}.{1} SET ".format(gConf['DATABASE'], gConf['TABLE'])
 
-    vCnt = vCur.execute('BEGIN')
+#    vCnt = vCur.execute('BEGIN')
+    print "BEGIN;"
     for r in xrange(1, vRowNum):
-        vCurRow = vSQLFmt + ", ".join([ "{0} = '{1}'".format(i.strip(), j.strip().encode('raw_unicode_escape')) if isinstance(j, basestring) else "{0} = {1}".format(i, j) for i, j in zip(vColName, vSheet.row_values(r)) ]) + ";"
-        print vCurRow
-        break
-    vCnt = vCur.execute('ROLLBACK')
+        vCurRow = vSQLFmt + ", ".join([ "{0} = '{1}'".format(i, j.strip().encode(gConf['CHARSET'])) if isinstance(j, unicode) else "{0} = {1}".format(i, j) for i, j in zip(vColName, vSheet.row_values(r)) ])
+        print vCurRow + ';'
+#    vCnt = vCur.execute(vCurRow)        
+#    vCnt = vCur.execute('ROLLBACK')
 
 
     
@@ -85,10 +98,15 @@ gConf['HOST'] = options.HOST
 gConf['USER'] = options.USER
 gConf['PSWD'] = options.PSWD
 gConf['DATABASE'] = options.DATABASE
+gConf['CHARSET'] = options.CHARSET
+gConf['DBCHARSET'] = options.DBCHARSET
 gConf['TABLE'] = options.TABLE
 gConf['WHERE'] = "WHERE {0}".format(options.WHERE) if options.WHERE else ''
 gConf['OUTFILE'] = options.OUTFILE if options.OUTFILE else "{0}.xls".format(options.TABLE)
 gConf['INFILE'] = options.INFILE if options.INFILE else "{0}.xls".format(options.TABLE)
+
+
+gConf['COLWIDTH'] = {2:3, 3:3, 4:3, 7:10, 10:5, 11:5, 12:10, 252:5, 253:10}
 gConf['XF'] = { -1:xlwt.easyxf(), 
                 7:xlwt.easyxf(num_format_str = 'yyyy-mm-dd hh:mm:ss'),
                 10:xlwt.easyxf(num_format_str = 'yyyy-mm-dd hh:mm:ss'),
@@ -96,7 +114,7 @@ gConf['XF'] = { -1:xlwt.easyxf(),
                 12:xlwt.easyxf(num_format_str = 'yyyy-mm-dd hh:mm:ss')
                 }
 
-dbConf = {'host':gConf['HOST'], 'port':3306, 'user':gConf['USER'], 'passwd':gConf['PSWD'], 'db':gConf['DATABASE'], 'charset':'latin1', 'cursorclass':MySQLdb.cursors.Cursor, 'autocommit':False}
+dbConf = {'host':gConf['HOST'], 'port':3306, 'user':gConf['USER'], 'passwd':gConf['PSWD'], 'db':gConf['DATABASE'], 'charset':gConf['DBCHARSET'], 'cursorclass':MySQLdb.cursors.Cursor, 'autocommit':False}
 
 
 try:
@@ -117,4 +135,7 @@ except Exception as e:
 finally:
     scur.close()
     sdb.close()
+
+
+
 
